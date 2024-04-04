9fa32f7184dc30a31b090e1dad845a0e0630a63d
OlwethuQzondi <164016147+OlwethuQzondi@users.noreply.github.com>
2024-04-04 14:27:04 +0200

    The application has been coded succesfully, the build has been generated and partially debugged.

--- app/src/main/java/com/st10447525/historymate/MainActivity.kt
+++ app/src/main/java/com/st10447525/historymate/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var resultTextView: TextView
     private lateinit var ageInput: EditText
     private lateinit var clearButton: Button
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
